5c69b28c63d7af12f881bce9db1deadc450cfc1f
StyxNT <1922188626@qq.com>
2021-08-03 20:43:55 +0800

    Initial commit

---
+++ vvt-server/src/main/java/com/styxnt/vvtserver/VvtServerApplication.java
@@ -0,0 +1,13 @@
+package com.styxnt.vvtserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VvtServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VvtServerApplication.class, args);
+    }
+
+}
---
+++ vvt-server/src/test/java/com/styxnt/vvtserver/VvtServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.styxnt.vvtserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VvtServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
